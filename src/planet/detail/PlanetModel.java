213c5c965f8625fde1cf096838d4437021235f7e
WoernerBro <seanwoerner.ru89@gmail.com>
2017-10-28 15:48:29 -0500

    Created nearly empty PlanetModel.java

---
+++ src/planet/detail/PlanetModel.java
@@ -0,0 +1,17 @@
+package planet.detail;
+
+
+public class PlanetModel {
+	private String planetImage;
+	private String planetName;
+	private float planetDiameterKM;
+	private float planetDiameterM;
+	private float planetMeanSurfaceTempC;
+	private float planetMeanSurfaceTempF;
+	private int planetNumberOfMoons;
+	private String fancyPlanetName;
+	
+	PlanetModel () {
+		
+	}
+}
