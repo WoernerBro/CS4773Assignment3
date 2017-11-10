c507eb5df7f147a8ec60d7da534c145ffd56951b
esnfish <esnfish@gmail.com>
2017-11-10 14:14:06 -0600

    stuff

---
+++ src/planet/detail/PlanetValidator.java
@@ -0,0 +1,40 @@
+package planet.detail;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PlanetValidator {
+	
+	
+	
+	public static boolean validatePlanetName(String testPlanetName) {
+		Pattern validCharacters = Pattern.compile("[^A-Za-z0-9 .-]");
+		Matcher invalidPlanetName = validCharacters.matcher(testPlanetName);
+		
+		if(testPlanetName.length() < 1 || testPlanetName.length() > 255)
+			return false;
+		if(invalidPlanetName.find())
+			return false;
+		return true;
+	}
+
+	public static boolean validateDiameter(float testPlanetDiameterKM) {
+		if(testPlanetDiameterKM <= 0 || testPlanetDiameterKM > 200000) {
+			System.out.println("Bad Diameter");
+			return false;
+		}
+		return true;
+	}
+	
+	public static boolean validateSurfaceTemperature(float testPlanetMeanSurfaceTempC) {
+		if(testPlanetMeanSurfaceTempC < -273.15 || testPlanetMeanSurfaceTempC > 500.0)
+			return false;
+		return true;
+	}
+	
+	public static boolean validNuberOfMoons(int testPlanetNumberOfMoons) {
+		if(testPlanetNumberOfMoons < 0 || testPlanetNumberOfMoons > 1000)
+			return false;
+		return true;
+	}
+}
