aa50fec8e3dbce7b81cd5b28be3346f49dad160d
Skorp <Skorp@Skorp-PC>
2017-11-11 22:27:03 -0600

    added img to appmain

--- src/app/AppMain.java
+++ src/app/AppMain.java
@@ -1,8 +1,12 @@
 package app;
 
+import java.awt.Toolkit;
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import planet.detail.Planet;
@@ -17,6 +21,7 @@ public class AppMain extends Application {
 		launch(args);
 	}
 	
+
 	//FXML startup method
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -27,10 +32,12 @@ public class AppMain extends Application {
 		FXMLLoader loader = new FXMLLoader(controller.getClass().getResource("PlanetView.fxml"));
 		loader.setController(controller);
 		
+		
 		Pane pane = (Pane) loader.load();
 
 		Scene scene = new Scene(pane, 590, 400);
 		primaryStage.setTitle("CS 4773 Assignment 3");
+		primaryStage.getIcons().add(new Image("https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/planet.png"));
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
